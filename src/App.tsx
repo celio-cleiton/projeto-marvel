99b10f4986aee18061e1c665b44f2d316fbbd56f
celio-cleiton <cleitonfreelance@gmail.com>
2023-10-17 21:09:48 -0300

    pagina home

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home/index"
+
+function App() {
+
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,95 @@
+'use client'
+import logo from '../../assets/Logo.png'
+import {
+  Box,
+  Flex,
+  Image,
+  HStack,
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Links = ['HOME', 'CHARACTERS', 'COMICS', 'EVENTS', 'CONTACT']
+
+const NavLink = (props: Props) => {
+  const { children } = props
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('#FFFFF', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Box>
+  )
+}
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Box bg={useColorModeValue('#EC1D24', '#FFFFF')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            color={'#FFFF'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box><Image src={logo} alt="Logo" /></Box>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'full'}
+                variant={'link'}
+                cursor={'pointer'}
+                minW={0}>
+                  <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+              </MenuButton>
+              
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4}><Text>Main Content Here</Text> </Box>
+    </>
+  )
+}
